9d5fb9add0a3a4c1a2c81a4d58e2c3c39bba9f3a
Levi <levi.xu@iubang.com>
2019-09-09 17:53:02 +0800

    add route config

---
+++ src/app/tank/component/rout-test/rout-test.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rout-test',
+  templateUrl: './rout-test.component.html',
+  styleUrls: ['./rout-test.component.scss']
+})
+
+export class RoutTestComponent implements OnInit {
+
+  constructor() {
+    return;
+  }
+
+  ngOnInit() {
+    return;
+  }
+}
---
+++ src/app/tank/component/ts-dom/ts-dom.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ts-dom',
+  templateUrl: './ts-dom.component.html',
+  styleUrls: ['./ts-dom.component.scss']
+})
+
+export class TsDomComponent implements OnInit {
+
+  constructor() {
+    return;
+  }
+
+  ngOnInit() {
+    return;
+  }
+}

f6f60e42101f253d0bc0957c5703311249ed9439
Levi <levi.xu@iubang.com>
2019-09-09 17:53:02 +0800

    add route config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TsDomComponent } from './tank/component/ts-dom/ts-dom.component';
+import { RoutTestComponent } from './tank/component/rout-test/rout-test.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: TsDomComponent,
+}, {
+  path: 'ts-dom',
+  component: TsDomComponent,
+}, {
+  path: 'route',
+  component: RoutTestComponent,
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TsDomComponent } from './tank/component/ts-dom/ts-dom.component';
+import { RoutTestComponent } from './tank/component/rout-test/rout-test.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     AppRoutingModule
+  ],
+  declarations: [
+    TsDomComponent,
+    AppComponent,
+    RoutTestComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]

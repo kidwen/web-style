c6d3aeaa393427dd09ceacdc819189348afd55ea
Levi <levi.xu@iubang.com>
2019-09-11 09:53:57 +0800

    rename progress to percentage

--- src/app/tank/component/ts-dom/ts-dom.component.ts
+++ src/app/tank/component/ts-dom/ts-dom.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ElementRef, AfterViewInit, ViewChild, Renderer2 } from '@angular/core';
+import { Component, ElementRef, AfterViewInit, ViewChild, Renderer2, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ts-dom',
@@ -7,14 +7,26 @@ import { Component, OnInit, ElementRef, AfterViewInit, ViewChild, Renderer2 } fr
 })
 
 export class TsDomComponent implements AfterViewInit {
+  @ViewChild('percentage', null)
+  public percentage?: ElementRef;
+
   @ViewChild('bar', null)
   public bar?: ElementRef;
+
+  @Input()
+  public percentageWidth?: number;
+
   constructor(
-    private elementRef: ElementRef,
     private renderer: Renderer2,
   ) { }
 
   public ngAfterViewInit() {
-    this.renderer.setStyle(this.bar.nativeElement, 'width', '5rem'); // 注意一定要有单位
+    if (this.percentageWidth) {
+      const percentageWidth: number = this.percentage.nativeElement.offsetWidth;
+      const barWidth: number = percentageWidth * this.percentageWidth;
+      this.renderer.setStyle(this.bar.nativeElement, 'width', `${barWidth}px`); // 注意一定要有单位
+    } else {
+      this.renderer.setStyle(this.bar.nativeElement, 'width', '5rem'); // 注意一定要有单位
+    }
   }
 }

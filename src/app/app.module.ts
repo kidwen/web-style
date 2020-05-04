72a2ea69db7293e2bb5dc8351aabff7b8eed6e4a
levi <levi.xu@iubang.com>
2020-05-05 00:15:53 +0800

    add css component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TankModule } from './tank/tank.module';
@@ -12,6 +13,7 @@ import { TankModule } from './tank/tank.module';
         RouterModule.forRoot([]),
         BrowserModule,
         TankModule,
+        BrowserAnimationsModule,
     ],
     bootstrap: [AppComponent],
 })
---
+++ src/modules/css-style/css-style.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'style-css-style',
+    templateUrl: './css-style.component.html',
+    styleUrls: ['./css-style.component.scss'],
+})
+export class CssStyleComponent { }

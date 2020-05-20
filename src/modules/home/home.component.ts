71dec024df3522263409340cba01824110536c75
Levi <levi.xu@iubang.com>
2020-05-20 17:38:33 +0800

    rxjs async scheduler

--- src/modules/home/home.component.ts
+++ src/modules/home/home.component.ts
@@ -8,7 +8,7 @@ import { MenuItem } from 'primeng/api';
 })
 
 export class HomeComponent implements OnInit {
-    public items: Array<MenuItem>;
+    public items?: Array<MenuItem>;
 
     public ngOnInit(): void {
         this.items = [

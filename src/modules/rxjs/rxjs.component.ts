b7aa0c7d51cac4561c470a0a5f2800704ce6040f
Levi <levi.xu@iubang.com>
2020-05-20 17:38:33 +0800

    rxjs async scheduler

--- src/modules/rxjs/rxjs.component.ts
+++ src/modules/rxjs/rxjs.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { asyncScheduler, Subscription } from 'rxjs';
 
 @Component({
     selector: 'style-rxjs',
     templateUrl: './rxjs.component.html',
     styleUrls: ['./rxjs.component.scss'],
 })
-export class RxjsComponent {
+export class RxjsComponent implements OnDestroy {
     public divContent?: string;
+    public sub?: Subscription;
 
-    public getI(): string {
-        return 'Click';
+    public getI(): void {
+        console.clear();
+        if (this.sub) {
+            this.sub.unsubscribe();
+        }
+        this.sub = asyncScheduler.schedule(task, 100, 0);
     }
+
+    public ngOnDestroy(): void {
+        this.sub?.unsubscribe();
+    }
+
+}
+
+function task(this: any, state: any): void {
+    console.info(state);
+    // tslint:disable-next-line: no-unsafe-any
+    this.schedule((state as number) + 1, 1000); // `this` references currently executing Action, which we reschedule with new state and delay
 }

a9c616a606db9e5e79d5174fcca80ef0450ac9b1
Levi <levi.xu@iubang.com>
2020-05-21 15:18:51 +0800

    add animal of rxjs

--- src/modules/rxjs/rxjs.component.ts
+++ src/modules/rxjs/rxjs.component.ts
@@ -1,31 +1,45 @@
-import { Component, OnDestroy } from '@angular/core';
-import { asyncScheduler, Subscription } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { animationFrameScheduler, asyncScheduler, SchedulerAction, Subscription } from 'rxjs';
 
+let div: HTMLDivElement | null = null;
 @Component({
     selector: 'style-rxjs',
     templateUrl: './rxjs.component.html',
     styleUrls: ['./rxjs.component.scss'],
 })
-export class RxjsComponent implements OnDestroy {
+export class RxjsComponent implements OnInit, OnDestroy {
     public divContent?: string;
-    public sub?: Subscription;
+    public sub$?: Subscription;
+    public animal$?: Subscription;
+
+    public ngOnInit(): void {
+        div = document.querySelector('div');
+        this.animal$ = animationFrameScheduler.schedule(addHeight, 0, 0);
+    }
 
     public getI(): void {
         console.clear();
-        if (this.sub) {
-            this.sub.unsubscribe();
+        if (this.sub$) {
+            this.sub$.unsubscribe();
         }
-        this.sub = asyncScheduler.schedule(task, 100, 0);
+        this.sub$ = asyncScheduler.schedule(task, 100, 0);
     }
 
     public ngOnDestroy(): void {
-        this.sub?.unsubscribe();
+        this.sub$?.unsubscribe();
+        this.animal$?.unsubscribe();
     }
 
 }
 
-function task(this: any, state: any): void {
+function task(this: SchedulerAction<number>, state: any): void {
     console.info(state);
-    // tslint:disable-next-line: no-unsafe-any
     this.schedule((state as number) + 1, 1000); // `this` references currently executing Action, which we reschedule with new state and delay
+}
+
+function addHeight(this: SchedulerAction<number>, height: any): void {
+    if (div) {
+        div.style.height = `${height}px`;
+        this.schedule((height as number) + 0.1);
+    }
 }

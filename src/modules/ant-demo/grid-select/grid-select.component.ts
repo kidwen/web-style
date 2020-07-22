bdf1f7562b618c2c003d5e2309b69eceae61da9b
kidwen <2061895077@qq.com>
2020-07-22 21:05:45 +0800

    add antd

---
+++ src/modules/ant-demo/grid-select/grid-select.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'selector-name',
+    templateUrl: './grid-select.component.html',
+    styleUrls: ['./grid-select.component.scss'],
+})
+
+export class GridSelectComponent implements OnInit {
+    public selectedValue: string = '';
+    public ngOnInit(): void {
+        return;
+    }
+}

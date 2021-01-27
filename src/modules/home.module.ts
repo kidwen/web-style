4c3b185d6453677d25c5f4431a74a4bb7cae48fc
kidwen <2061895077@qq.com>
2021-01-28 00:13:49 +0800

    rm primeng

--- src/modules/home.module.ts
+++ src/modules/home.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { NzSelectModule } from 'ng-zorro-antd/select';
-import { ButtonModule } from 'primeng/button';
-import { MenuModule } from 'primeng/menu';
 import { GridSelectComponent } from './ant-demo/grid-select/grid-select.component';
 import { CssStyleComponent } from './css-style/css-style.component';
 import { directives } from './directives';
@@ -15,10 +13,8 @@ import { RxjsComponent } from './rxjs/rxjs.component';
 
 @NgModule({
     imports: [
-        ButtonModule,
         CommonModule,
         HomeRoutingModule,
-        MenuModule,
         FormsModule,
         NzGridModule,
         NzSelectModule,

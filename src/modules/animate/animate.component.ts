01913440eba94c03b77d9c75b69cb61546499be5
kidwen <2061895077@qq.com>
2021-02-06 19:42:43 +0800

    add animate css

---
+++ src/modules/animate/animate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-animate',
+    templateUrl: './animate.component.html',
+    styleUrls: ['./animate.component.scss'],
+})
+
+export class AnimateComponent {
+
+}

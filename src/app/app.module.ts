62aed720bf6bd9e829bf1c7402a9f1dce0ac1068
kidwen <2061895077@qq.com>
2021-05-01 00:48:15 +0800

    replace menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import zh from '@angular/common/locales/zh';
+import { default as zh } from '@angular/common/locales/zh';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
@@ -22,7 +21,6 @@ registerLocaleData(zh);
         BrowserModule,
         TankModule,
         BrowserAnimationsModule,
-        FormsModule,
         HttpClientModule,
         HomeModule,
     ],
--- src/app/tank/tank.module.ts
+++ src/app/tank/tank.module.ts
@@ -1,17 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
 import { MainComponent } from './components/main/main.component';
+import { StyleMenuNavComponent } from './components/menu-nav/menu-nav.component';
 import { NotFoundComponent } from './components/not-found/not-fount.component';
 import { TankRoutingModule } from './tank-routing.module';
 
 @NgModule({
     imports: [
         TankRoutingModule,
+        MatListModule,
+        FormsModule,
+        CommonModule,
     ],
     declarations: [
         MainComponent,
         NotFoundComponent,
+        StyleMenuNavComponent,
     ],
-    exports: [],
+    exports: [StyleMenuNavComponent],
 })
 
 export class TankModule { }

9b7ffc36ace93dad9bf00acde9457742d627c903
kidwen <2061895077@qq.com>
2021-05-03 22:56:24 +0800

    update route path

--- projects/layout/src/modules/navbar/navbar.component.ts
+++ projects/layout/src/modules/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatButtonProperty } from '@kidwen/layout';
 
 @Component({
     selector: 'style-navbar',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 
 export class NavbarComponent {
+
+    @Input()
+    public links?: Array<MatButtonProperty>;
+
 }

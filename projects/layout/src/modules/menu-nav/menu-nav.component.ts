d9dfbd941a47cbafd64533d677416b3a9087f091
kidwen <2061895077@qq.com>
2021-05-04 21:08:35 +0800

    update menu nav bar

--- projects/layout/src/modules/menu-nav/menu-nav.component.ts
+++ projects/layout/src/modules/menu-nav/menu-nav.component.ts
@@ -1,18 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output, AfterContentInit, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { MatButtonProperty } from '@kidwen/layout';
-
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 @Component({
     selector: 'style-menu-nav',
     templateUrl: './menu-nav.component.html',
     styleUrls: ['./menu-nav.component.scss'],
 })
 
-export class MenuNavComponent {
+export class MenuNavComponent implements AfterContentInit, OnDestroy {
 
-    @Input()
     public menuSelected?: string;
 
     @Input()
     public links?: Array<MatButtonProperty>;
+
+    @Output()
+    public onSelected: EventEmitter<string> = new EventEmitter<string>();
+
+    private url?: string;
+
+    private sub?: Subscription;
+
+    public constructor(
+        private router: Router,
+    ) {
+        this.sub = this.router.events.pipe(
+            filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+        ).subscribe(e => {
+            this.url = e.url;
+        });
+    }
+
+    public ngAfterContentInit(): void {
+        let link = this.links?.find(link => link.routerLink == this.url);
+        this.menuSelected = link?.text;
+    }
+
+    public menuClick(text: string): void {
+        this.menuSelected = text;
+        this.onSelected.emit(text);
+    }
+
+    public ngOnDestroy(): void {
+        this.sub?.unsubscribe();
+    }
 }
 
--- src/app/tank/components/main/main.component.ts
+++ src/app/tank/components/main/main.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component, OnDestroy } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { MatButtonProperty } from '@kidwen/layout';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
     selector: 'style-main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements AfterContentInit, OnDestroy {
+
     public links: Array<MatButtonProperty> = [{
         routerLink: '/demos/home',
-        text: 'home',
+        text: 'Home',
     }, {
         routerLink: '/demos/rxjs',
         text: 'rxjs',
@@ -26,4 +30,33 @@ export class MainComponent {
         routerLink: '/demos/ant-grid',
         text: 'ant-grid',
     }];
+
+    public selectedMenu?: string;
+
+    private sub?: Subscription;
+
+    private url?: string;
+
+    public constructor(
+        private router: Router,
+    ) {
+        this.sub = this.router.events.pipe(
+            filter((e): e is NavigationEnd => e instanceof NavigationEnd),
+        ).subscribe(e => {
+            this.url = e.url;
+        });
+    }
+
+    public ngAfterContentInit(): void {
+        let link = this.links?.find(link => link.routerLink == this.url);
+        this.selectedMenu = link?.text;
+    }
+
+    public onSelected(e: string): void {
+        this.selectedMenu = e;
+    }
+
+    public ngOnDestroy(): void {
+        this.sub?.unsubscribe();
+    }
 }

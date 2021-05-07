6ab37b6ab8187c6f3ef1c32fff9739bcb60b9f83
kidwen <2061895077@qq.com>
2021-05-07 23:16:31 +0800

    fix selected menu text show when refresh url

--- src/app/tank/components/main/main.component.ts
+++ src/app/tank/components/main/main.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { MatButtonProperty } from '@kidwen/layout';
 import { Subscription } from 'rxjs';
@@ -9,33 +9,32 @@ import { filter } from 'rxjs/operators';
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
 })
-export class MainComponent implements AfterContentInit, OnDestroy {
+export class MainComponent implements OnDestroy {
 
     public links: Array<MatButtonProperty> = [{
-        routerLink: '/demos/home',
-        text: 'Home',
+        routerLink: '/demos/shadow',
+        text: 'Shadow',
     }, {
         routerLink: '/demos/rxjs',
-        text: 'rxjs',
+        text: 'Rxjs',
     }, {
         routerLink: '/demos/position',
-        text: 'position',
+        text: 'Position',
     }, {
         routerLink: '/demos/css',
-        text: 'css',
+        text: 'Css',
     }, {
         routerLink: '/demos/animate',
-        text: 'animate',
+        text: 'Animate',
     }, {
         routerLink: '/demos/ant-grid',
-        text: 'ant-grid',
+        text: 'Ant-grid',
     }];
 
-    public selectedMenu?: string;
+    public selectedMenu: string = 'Intro';
 
     private sub?: Subscription;
 
-    private url?: string;
 
     public constructor(
         private router: Router,
@@ -43,13 +42,8 @@ export class MainComponent implements AfterContentInit, OnDestroy {
         this.sub = this.router.events.pipe(
             filter((e): e is NavigationEnd => e instanceof NavigationEnd),
         ).subscribe(e => {
-            this.url = e.url;
+            this.selectedMenu = this.links.find(l => l.routerLink == e.url)?.text ?? this.selectedMenu;
         });
-    }
-
-    public ngAfterContentInit(): void {
-        let link = this.links?.find(link => link.routerLink == this.url);
-        this.selectedMenu = link?.text;
     }
 
     public onSelected(e: string): void {
--- src/app/tank/tank.module.ts
+++ src/app/tank/tank.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MenuNavModule } from '@kidwen/layout';
+import { IntroComponent } from './components/intro/intro.component';
 import { MainComponent } from './components/main/main.component';
 import { NotFoundComponent } from './components/not-found/not-fount.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
@@ -22,6 +23,7 @@ import { TankRoutingModule } from './tank-routing.module';
         MainComponent,
         NotFoundComponent,
         WelcomeComponent,
+        IntroComponent,
     ],
     exports: [],
 })

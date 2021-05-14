dd39b351e588304f2f25d9d0b0cf9c12a15660be
kidwen <2061895077@qq.com>
2021-05-15 00:24:37 +0800

    fix lint

--- src/main.ts
+++ src/main.ts
@@ -8,4 +8,6 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
+    .catch(err => {
+        return console.error(err);
+    });

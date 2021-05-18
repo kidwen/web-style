b9bbba31f2f6ff3de6be0466dd1c5c24d84c7993
kidwen <xuqiujia2020@outlook.com>
2021-05-18 22:53:11 +0800

    update welcome page style

--- src/app/tank/components/welcome/welcome.component.ts
+++ src/app/tank/components/welcome/welcome.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'style-welcome',
     templateUrl: './welcome.component.html',
+    styleUrls: ['./welcome.component.scss'],
 })
 
 export class WelcomeComponent {

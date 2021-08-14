cb691bf1d6dbb786d6182e42e75b8b830d220d59
kidwen <xuqiujia2020@outlook.com>
2021-08-14 23:49:38 +0800

    add center layout page

---
+++ src/modules/center/center.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-center',
+    templateUrl: './center.component.html',
+    styleUrls: ['./center.component.scss'],
+})
+
+export class CenterComponent {
+
+}

9db37830f5259c54c519930f91ff05e195c1c69f
kidwen <xuqiujia2020@outlook.com>
2021-09-08 22:16:18 +0800

    update carousel style

--- projects/layout/src/modules/carousel/carousel.module.ts
+++ projects/layout/src/modules/carousel/carousel.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +8,7 @@ import { CarouselComponent } from './carousel.component';
     imports: [
         MatButtonModule,
         MatIconModule,
+        CommonModule,
     ],
     exports: [
         CarouselComponent,

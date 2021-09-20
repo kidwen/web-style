a87b1b477a4999773ab1f27f36e1b0935a4cd741
kidwen <xuqiujia2020@outlook.com>
2021-09-21 00:30:00 +0800

    add table demo

--- src/modules/home.module.ts
+++ src/modules/home.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatTabsModule } from '@angular/material/tabs';
+import { TableModule } from '@kidwen/layout';
 import { AnimateComponent } from './animate/animate.component';
 import { CenterComponent } from './center/center.component';
 import { CssStyleComponent } from './css-style/css-style.component';
@@ -10,6 +11,7 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { PositionComponent } from './position/position.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { TableDemoComponent } from './table/table-demo.component';
 
 @NgModule({
     imports: [
@@ -17,6 +19,7 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         HomeRoutingModule,
         FormsModule,
         MatTabsModule,
+        TableModule,
     ],
     declarations: [
         AnimateComponent,
@@ -25,6 +28,7 @@ import { RxjsComponent } from './rxjs/rxjs.component';
         HomeComponent,
         PositionComponent,
         RxjsComponent,
+        TableDemoComponent,
         ...directives,
     ],
 })

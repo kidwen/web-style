2df612b806ef1a0c69aece760de28a61e2cb16bf
kidwen <xuqiujia2020@outlook.com>
2022-01-02 14:28:22 +0800

    add change detection `onPush` demo

---
+++ src/modules/chang-detection/child/child.component.ts
@@ -0,0 +1,14 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { User } from 'src/model/user.model';
+
+@Component({
+    selector: 'style-child',
+    templateUrl: './child.component.html',
+    styleUrls: ['./child.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+})
+
+export class ChildComponent {
+    @Input()
+    public user: User = { name: 'kidwen' };
+}

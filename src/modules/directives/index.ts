e25fe8571770fc8308a7bc23fc1fb6044ce0088a
kidwen <xuqiujia2020@outlook.com>
2022-08-21 12:30:07 +0800

    fix build

--- src/modules/directives/index.ts
+++ src/modules/directives/index.ts
@@ -1,7 +1,8 @@
 import { IteratorDirective } from './iterator.directive';
 import { GreeterDirective } from './greeter.directive';
 
-export let directives: Array<unknown> = [
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export let directives: Array<any> = [
     IteratorDirective,
     GreeterDirective,
 ];

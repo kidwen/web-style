2a06a869e4415606831b1ab860ed3e5f7aaa0199
NieRiFeng <17673376360@163.com>
2023-11-16 15:15:42 +0800

    Replace ngFor with @for

--- projects/layout/src/modules/carousel/carousel.component.ts
+++ projects/layout/src/modules/carousel/carousel.component.ts
@@ -3,7 +3,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
     selector: 'lib-carousel',
     templateUrl: './carousel.component.html',
-    styleUrls: ['./carousel.component.scss'],
+    styleUrl: './carousel.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
--- projects/layout/src/modules/navbar/navbar.component.ts
+++ projects/layout/src/modules/navbar/navbar.component.ts
@@ -1,16 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-import { MatButtonProperty } from '../../model/mat-button.model';
+import { ChangeDetectionStrategy, Component, Input } from "@angular/core";
+import { MatButtonProperty } from "../../model/mat-button.model";
 
 @Component({
-    selector: 'lib-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.scss'],
+    selector: "lib-navbar",
+    templateUrl: "./navbar.component.html",
+    styleUrl: "./navbar.component.scss",
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class NavbarComponent {
-
     @Input()
     public links?: Array<MatButtonProperty>;
-
 }

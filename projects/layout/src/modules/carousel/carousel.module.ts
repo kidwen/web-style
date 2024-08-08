ba046069e2c4e8606017bf452481452b27c5bbca
kidwen <xuqiujia2020@outlook.com>
2024-08-08 23:41:45 +0800

    add horizontal scroll

--- projects/layout/src/modules/carousel/carousel.module.ts
+++ projects/layout/src/modules/carousel/carousel.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { CarouselComponent } from './carousel.component';
+import { CarouselItem } from './carousel-item.directive';
 
 @NgModule({
     imports: [
@@ -12,9 +13,11 @@ import { CarouselComponent } from './carousel.component';
     ],
     exports: [
         CarouselComponent,
+        CarouselItem,
     ],
     declarations: [
         CarouselComponent,
+        CarouselItem,
     ],
 })
 export class CarouselModule { }

f2fa21adf55519552c7d75d1551462d116468d97
kidwen <xuqiujia2020@outlook.com>
2024-08-08 09:42:42 +0800

    add carousel directive

---
+++ projects/layout/src/modules/carousel/carousel-item.directive.ts
@@ -0,0 +1,21 @@
+import { FocusableOption } from "@angular/cdk/a11y";
+import { Directive, HostBinding, ElementRef } from "@angular/core";
+
+@Directive({
+    selector: '[carousel-item]',
+    standalone: true,
+})
+export class CarouselItem implements FocusableOption {
+    @HostBinding('attr.role')
+    public readonly role: string = 'listitem';
+
+    @HostBinding('tabindex')
+    public tabindex: number = -1;
+
+    public constructor(
+        public readonly element: ElementRef<HTMLElement>,
+    ) { }
+    public focus(): void {
+        this.element.nativeElement.focus({ preventScroll: true });
+    }
+}

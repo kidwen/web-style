2d2ce844695b50205396657e949bfa6e2080db26
kidwen <xuqiujia2020@outlook.com>
2024-08-19 18:14:02 +0800

    add replay subject code

--- src/modules/rxjs/rxjs.component.ts
+++ src/modules/rxjs/rxjs.component.ts
@@ -13,6 +13,26 @@ export class RxjsComponent implements OnInit, OnDestroy {
     @ViewChild('animal', { static: true })
     public animalDiv?: ElementRef; public divContent?: string;
 
+    public code =
+`public data$: ReplaySubject<string> = new ReplaySubject<string>(2);
+
+this.data$.next('data 1');
+this.data$.next('data 2');
+this.data$.next('data 3');
+
+this.data$.pipe(takeUntil(this.onDestroy$)).subscribe({
+    next: data => {
+        this.sub1.push(\`Subscribe 1: \${data}\`);
+    },
+});
+
+this.data$.pipe(takeUntil(this.onDestroy$)).subscribe({
+    next: data => {
+        this.sub2.push(\`Subscribe 2: \${data}\`);
+    },
+});
+`;
+
     public sub1: Array<string> = new Array<string>();
 
     public sub2: Array<string> = new Array<string>();

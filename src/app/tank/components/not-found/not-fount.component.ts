d2c663e505688450ccdab6c4a1025f52a3d9f009
kidwen <xuqiujia2020@outlook.com>
2024-08-20 23:46:40 +0800

    fix 404 page

--- src/app/tank/components/not-found/not-fount.component.ts
+++ src/app/tank/components/not-found/not-fount.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
     selector: 'style-not-fount',
@@ -8,12 +8,18 @@ import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/
 })
 export class NotFoundComponent implements OnInit, OnDestroy {
     public countDown: number = 5;
+
     private intervalId?: number;
+
+    public constructor(
+        private cdr: ChangeDetectorRef,
+    ) { }
 
     public ngOnInit(): void {
         this.intervalId = window.setInterval(() => {
             if (this.countDown > 1) {
                 this.countDown -= 1;
+                this.cdr.markForCheck();
             } else {
                 history.back();
                 clearInterval(this.intervalId);
--- src/modules/css-style/css-style.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-    selector: 'style-css-style',
-    templateUrl: './css-style.component.html',
-    styleUrls: ['./css-style.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class CssStyleComponent { }

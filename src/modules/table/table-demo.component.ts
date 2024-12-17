2039c53a766a9881670551d4d9f797062a06e405
kidwen <xuqiujia2020@outlook.com>
2024-12-17 19:27:51 +0800

    update intro page

--- src/modules/table/table-demo.component.ts
+++ src/modules/table/table-demo.component.ts
@@ -1,10 +1,22 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal, WritableSignal } from '@angular/core';
+import { User } from 'src/models';
 
 @Component({
     selector: 'app-table-demo',
     templateUrl: './table-demo.component.html',
+    styleUrl: './table-demo.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 
 export class TableDemoComponent {
+
+    public users: WritableSignal<Array<User>> = signal<Array<User>>([{
+        id: '001',
+        name: 'kidwen',
+        age: 18,
+    }, {
+        id: '002',
+        name: 'nick',
+        age: 20,
+    }]);
 }

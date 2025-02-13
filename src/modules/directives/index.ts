e33b44f2067dbd9c5e9a377996d983ee0462dc98
xuqj2 <xuqj2@lenovo.com>
2025-02-13 15:42:35 +0800

    Convert all text files to LF line endings

--- src/modules/directives/index.ts
+++ src/modules/directives/index.ts
@@ -2,7 +2,7 @@ import { IteratorDirective } from './iterator.directive';
 import { GreeterDirective } from './greeter.directive';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export let directives: Array<any> = [
+export const directives: Array<any> = [
     IteratorDirective,
     GreeterDirective,
 ];

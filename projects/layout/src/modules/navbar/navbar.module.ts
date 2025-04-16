c1fda624b3d9144b1c0bfbd2c557ff8856abb2da
kidwen <xuqiujia2020@outlook.com>
2025-04-16 10:48:07 +0800

    add dark mode

--- projects/layout/src/modules/navbar/navbar.module.ts
+++ projects/layout/src/modules/navbar/navbar.module.ts
@@ -1,16 +1,13 @@
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './navbar.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterModule } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
+import { NavbarComponent } from "./navbar.component";
 
 @NgModule({
-    imports: [
-        MatButtonModule,
-        CommonModule,
-        RouterModule,
-    ],
+    imports: [MatButtonModule, CommonModule, RouterModule, MatIconModule],
     exports: [NavbarComponent],
     declarations: [NavbarComponent],
 })
-export class NavbarModule { }
+export class NavbarModule {}

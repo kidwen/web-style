6930a2733794122c07c13ed8e42d45e0c1bcd044
kidwen <xuqiujia2020@outlook.com>
2025-07-14 13:45:59 +0800

    add app config file

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,13 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-
-import { provideHighlightOptions } from 'ngx-highlightjs';
-import { provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
-import { withRouterConfig, provideRouter } from '@angular/router';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { enableProdMode } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
 import { environment } from './environments/environment';
-import { TankModule } from './app/tank/tank.module';
-import { HomeModule } from './modules/home.module';
 import { AppComponent } from './app/app.component';
 
 if (environment.production) {
     enableProdMode();
 }
 
-bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, TankModule, HomeModule),
-        provideHighlightOptions({
-            fullLibraryLoader: () => import('highlight.js'),
-            lineNumbersLoader: () => import('ngx-highlightjs/line-numbers'),
-        }),
-        provideHttpClient(withInterceptorsFromDi()),
-        provideRouter(
-            [],
-            withRouterConfig({ canceledNavigationResolution: 'computed' }),
-        ),
-        provideAnimations(),
-    ],
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch(err => 
+    console.error(err),
+);
